34ea613ab56f9c494b7eec2e938647402c2cb448
ozgurgkmn <ozgurgokmen@yesilgezegen.com>
2024-08-05 11:47:33 +0300

    Added : RemoteMigrations

---
+++ DataAccessLayer/Migrations/RemoteMigrations/20240805065929_TestMigration2.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataAccessLayer.Migrations.RemoteMigrations
+{
+    /// <inheritdoc />
+    public partial class TestMigration2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Tests",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tests", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Tests");
+        }
+    }
+}
