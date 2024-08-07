88856a8496e279835a4f5a3cfb3cd5a020cdba26
CNevzat <ncirpicioglu@gmail.com>
2024-08-07 13:38:21 +0300

    Fixed : Contract Version

--- Shared/Interface/IContractRepository.cs
+++ Shared/Interface/IContractRepository.cs
@@ -1,3 +1,4 @@
+using System.Linq.Expressions;
 using Shared.DTO.Contract.Request;
 using Shared.DTO.Contract.Response;
 
@@ -7,7 +8,7 @@ public interface IContractRepository
 {
     Task<ContractResponse> CreateContract(CreateContractRequest createContractRequest);
     
-    Task<List<GetContractsResponse>> GetContracts();
+    Task<List<GetContractsResponse>> GetContracts(Expression<Func<GetContractsResponse, bool>>? predicate = null);
     
     Task<GetContractResponse> GetContract(Guid contractId); 
 }
