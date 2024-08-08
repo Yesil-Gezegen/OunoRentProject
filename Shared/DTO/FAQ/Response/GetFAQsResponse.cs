7ccbc2a3beadf0644d38ab8d80129be95d4865eb
ozgurgkmn <ozgurgokmen@yesilgezegen.com>
2024-08-08 10:14:33 +0300

    Add : Text property to GetFAQsResponse.cs

--- Shared/DTO/FAQ/Response/GetFAQsResponse.cs
+++ Shared/DTO/FAQ/Response/GetFAQsResponse.cs
@@ -4,6 +4,7 @@ public record GetFAQsResponse
 {
     public Guid FAQId { get; set; }
     public string Label { get; set; }
+    public string Text { get; set; }
     public int OrderNumber { get; set; }
     public bool IsActive { get; set; }
 }
